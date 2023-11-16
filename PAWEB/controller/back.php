fc3df3b7cc4e07540432bc4b7020bc21f443db98
STressNGoding <93185675+DimasYudha1223@users.noreply.github.com>
2023-11-16 18:05:02 +0800

    Delete PAWEB directory

--- PAWEB/controller/back.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$id_artikel = $_GET['id']; 
-echo " <script>
-document.location.href = '../read.php?id=$id_artikel';
-</script>";
-?>
--- PAWEB/controller/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script>
-    alert('Anda telah logout');
-    window.location.href = '../index.php';
-</script>"
-?>
